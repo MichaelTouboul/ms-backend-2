178ded87748facb4c78fb8754c355e59283c2a4f
Michael Touboul <mica.touboul@gmail.com>
2026-01-13 16:37:36 +0200

    [MONGO-DB] Mongoose integration

--- apps/sls-template/src/app.controller.ts
+++ apps/sls-template/src/app.controller.ts
@@ -1,13 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Logger, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
+  constructor(
+    @InjectConnection() private readonly mongoConnection: Connection,
+    @Inject(AppService) private readonly appService: AppService,
+  ) {}
 
   @Get('health')
   health() {
-    return { ok: true, nodemon: 'ok' };
+    try {
+      const mongoState = this.mongoConnection.readyState;
+
+      const mongoStatusMap: Record<number, string> = {
+        0: 'disconnected',
+        1: 'connected',
+        2: 'connecting',
+        3: 'disconnecting',
+      };
+      return {
+        status: 'ok',
+        mongo: {
+          state: mongoStatusMap[mongoState] ?? 'unknown',
+        },
+      };
+    } catch (e) {
+      this.logger.log(e);
+      return e;
+    }
+  }
+
+  @Post()
+  async create(@Body() payload: any) {
+    // TODO: Create dto
+    return await this.appService.create(payload);
   }
 }
--- apps/sls-template/src/app.module.ts
+++ apps/sls-template/src/app.module.ts
@@ -1,9 +1,35 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Template, TemplateSchema } from './schema/template.schema';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const uri = config.get<string>('MONGODB_URI');
+
+        if (!uri) {
+          throw new Error('MONGODB_URI is not defined');
+        }
+
+        return {
+          uri,
+          serverSelectionTimeoutMS: 5000,
+        };
+      },
+    }),
+    MongooseModule.forFeature([
+      { name: Template.name, schema: TemplateSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
